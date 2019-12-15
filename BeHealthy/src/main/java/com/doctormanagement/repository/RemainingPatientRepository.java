3982d2729bfaaa0e4527f3cd269e46b7d8929abc
nayeemahmed24 <42116405+nayeemahmed24@users.noreply.github.com>
2019-12-15 15:25:14 +0600

    bug fixed and appointment calander config

--- BeHealthy/src/main/java/com/doctormanagement/repository/RemainingPatientRepository.java
+++ BeHealthy/src/main/java/com/doctormanagement/repository/RemainingPatientRepository.java
@@ -1,11 +1,14 @@
 package com.doctormanagement.repository;
 
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 
 import com.doctormanagement.model.RemainingPatient;
-
+import com.doctormanagement.model.RemainingPatient;
 public interface RemainingPatientRepository extends CrudRepository<RemainingPatient, Long>{
 	RemainingPatient findByDoctorphonenumberAndDate(String phonenumber,String date);
+	List<RemainingPatient> findByDoctorphonenumber(String doctorphonenumber);
 }
